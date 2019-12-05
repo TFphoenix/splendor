486cafd97687d461d231f0e73564e000a45a212f
Eugen <motoc.eugen@yahoo.com>
2019-12-05 13:21:38 +0200

    Fixed bug with instantiating Token class. Modified header of functions using Token as argument.

--- Splendor/Splendor/Hand.cpp
+++ Splendor/Splendor/Hand.cpp
@@ -1,13 +1,13 @@
 #include "Hand.h"
 
-Hand::Hand(std::vector<std::optional<ExpansionCard>> expansionCard, std::vector<std::optional<IToken>> token): m_expansionCard(expansionCard),m_token(token)
+Hand::Hand(std::vector<std::optional<ExpansionCard>> expansionCard, std::vector<std::optional<Token>> token): m_expansionCard(expansionCard),m_token(token)
 {
 }
 
-void Hand::AddToken(std::vector<std::optional<IToken>> token)
+void Hand::AddToken(std::vector<std::optional<Token>> token)
 {
 	if (m_token.size() <= 7)
-		m_token == token;
+		m_token = token;
 	if (m_token.size() == 8 && token.size() == 2)
 		m_token = token;
 }
--- Splendor/Splendor/Hand.h
+++ Splendor/Splendor/Hand.h
@@ -1,20 +1,20 @@
 #pragma once
 #include<vector>
 #include"ExpansionCard.h"
-#include"IToken.h"
+#include"Token.h"
 #include"optional"
 class Hand
 {
 public:
-	Hand(std::vector<std::optional<ExpansionCard>> expansionCard, std::vector<std::optional<IToken>> token);
-	void AddToken(std::vector<std::optional<IToken>>token);
+	Hand(std::vector<std::optional<ExpansionCard>> expansionCard, std::vector<std::optional<Token>> token);
+	void AddToken(std::vector<std::optional<Token>>token);
 	void AddExpansionCard(std::optional<ExpansionCard > card);
 	int	GetSizeOfToken();
 	int	GetSizeOfExpansionCard();
 	void RemoveToken();
 	
 private:
-	std::vector<std::optional<IToken>> m_token;
+	std::vector<std::optional<Token>> m_token;
 	std::vector<std::optional<ExpansionCard>> m_expansionCard;
 };
 
