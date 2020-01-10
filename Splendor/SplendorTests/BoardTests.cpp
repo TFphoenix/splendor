1e4d3a0311f5891bfbf232e76e4bfa7315d314f4
a3drian <38135936+a3drian@users.noreply.github.com>
2020-01-10 15:39:39 +0200

    Added SplendorTests Unit Testing Project with included dependencies and .obj files. Tested Board initialization.

---
+++ Splendor/SplendorTests/BoardTests.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "Board.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SplendorTests
+{
+	TEST_CLASS(BoardTests)
+	{
+	public:
+
+		TEST_METHOD(InitializeBoard) {
+			Board board;
+		}
+
+	};
+}
