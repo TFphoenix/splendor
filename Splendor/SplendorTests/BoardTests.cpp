45a50a8f8a7a43224e7ede65a1faa381f4e8cb59
a3drian <38135936+a3drian@users.noreply.github.com>
2020-01-10 17:33:02 +0200

    Added tests for checking token data on Board initialization. Included test class for PregameSession.

--- Splendor/SplendorTests/BoardTests.cpp
+++ Splendor/SplendorTests/BoardTests.cpp
@@ -5,6 +5,8 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+const int numberOfTokens = 5;
+
 namespace SplendorTests
 {
 	TEST_CLASS(BoardTests)
@@ -13,6 +15,32 @@ namespace SplendorTests
 
 		TEST_METHOD(InitializeBoard) {
 			Board board;
+		}
+
+		TEST_METHOD(CheckTokenDataSize)
+		{
+			Board board;
+			const std::unordered_map<IToken::Type, uint16_t> tokenData = board.GetTokensData();
+
+			Assert::IsTrue(tokenData.size() == numberOfTokens, L"Number of tokens is not equal to 5!");
+		}
+
+		TEST_METHOD(CheckTokenDataElements)
+		{
+			Board board;
+			const std::unordered_map<IToken::Type, uint16_t> tokenData = board.GetTokensData();
+
+			int tokens[5] = { 5, 0, 1, 2, 3 };	//Gold, BlackOnyx, GreenEmerald, WhiteDiamond, BlueSapphire
+
+			int counter = 0;
+			for (const auto token : tokenData) {
+				IToken::Type type = token.first;
+				const int enumNumber = static_cast<int>(type);
+				if(enumNumber != tokens[counter++])
+				{
+					Assert::Fail(L"Tokens were not loaded in order!");
+				}
+			}
 		}
 
 	};
---
+++ Splendor/SplendorTests/PregameSessionsTests.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "PregameSetup.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SplendorTests
+{
+	TEST_CLASS(PregameSessionsTests)
+	{
+	public:
+
+	};
+}
