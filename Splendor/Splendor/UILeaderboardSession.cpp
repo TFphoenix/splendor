4fd099a8bd8951fb46596fae75d49b3bd9e3a03c
Bogdan <bogdan.kocsis@gmail.com>
2020-01-18 04:46:26 +0200

    Added UIText. Made a loader function to read files like test1 and leaderboard.

--- Splendor/Splendor/UILeaderboardSession.cpp
+++ Splendor/Splendor/UILeaderboardSession.cpp
@@ -1,7 +1,7 @@
 #include "UILeaderboardSession.h"
 
 UILeaderboardSession::UILeaderboardSession(const sf::Vector2u& windowSize) :
-	m_gameModePanel("First place", UIOptionsPanel::Type::Radio,
+	m_gameModePanel("", UIOptionsPanel::Type::Radio,
 		sf::Vector2f(50.0f, 0.2f * windowSize.y + 30.0f),
 		sf::Vector2f(windowSize.x - 50.0f, 0.15f * windowSize.y))
 {
@@ -24,6 +24,12 @@ UILeaderboardSession::UILeaderboardSession(const sf::Vector2u& windowSize) :
 	button.setPosition(windowSize.x - 350.0f, windowSize.y - 130.0f);
 	m_backToMenuButton = new UIButton(button);
 	m_backToMenuButton->ChangeText("Main Menu");
+
+	//Text
+	m_labelText = new UIText(sf::Vector2f(150.0f, 0.3* windowSize.y + 30.0f), UIText::TextAlign::mid_center, UIText::AvailableFonts::DosisLight, "First Place", 60);
+	m_labelText->setFillColor(UIColors::GoldYellow);
+	m_labelText->setOutlineThickness(3.0f);
+	m_labelText->AlignText(UIText::TextAlign::mid_center);
 }
 
 UILeaderboardSession::~UILeaderboardSession()
@@ -35,9 +41,10 @@ void UILeaderboardSession::draw(sf::RenderTarget& target, sf::RenderStates state
 {
 	target.draw(m_titleBackground);
 	target.draw(m_title);
+	target.draw(*m_labelText);
 	target.draw(m_gameModePanel);
 	target.draw(*m_backToMenuButton);
-
+	
 }
 
 void UILeaderboardSession::PassEvent(const sf::Event& event)
@@ -54,3 +61,26 @@ UILeaderboardSession::Events UILeaderboardSession::GetEvent() const
 	}
 	return Events::None;
 }
+
+bool SortBySec(const std::pair<std::string, uint16_t>& a, const std::pair<std::string, uint16_t>& b)
+{
+	
+		return (a.second < b.second);
+	
+}
+
+void UILeaderboardSession::LoadFromFile()
+{
+
+	std::ifstream readFile(s_leaderboardFile);
+	std::string name;
+	uint16_t number;
+	while (readFile >> name >> number)
+	{
+		s_leaderboard.push_back(std::make_pair(name, number));
+	}
+
+	std::sort(s_leaderboard.begin(), s_leaderboard.end(), SortBySec);
+
+}
+
